4d16e12fb94a2c104c6696e4c445ed50d29cccd4
Jeremy Price <donomii@gmail.com>
2018-11-17 15:41:21 +0100

    Reformat

--- throff.go
+++ throff.go
@@ -1,19 +1,16 @@
 package main
+
 import "github.com/donomii/throfflib"
 
-
-
 import (
-	"os"
 	"fmt"
 	_ "net/http/pprof"
+	"os"
 )
 
-
-
-func main () {
+func main() {
 	go func() {
-			//log.Println(http.ListenAndServe("localhost:6060", nil))
+		//log.Println(http.ListenAndServe("localhost:6060", nil))
 	}()
 	t := throfflib.MakeEngine()
 	t = throfflib.LoadGraphics(t)
@@ -21,6 +18,7 @@ func main () {
 	strs := os.Args[1:]
 	//t = t.RunFile("bootstrapgo.lib")
 	t = t.RunString(throfflib.BootStrapString(), "Internal Bootstrap")
+
 	if len(strs) == 0 {
 		t = t.RunString("PRINTLN [ Welcome to the THROFF command shell v0.1.  Type HELP for help. ]", "repl")
 		throfflib.Repl(t)
@@ -29,7 +27,8 @@ func main () {
 		fmt.Printf("")
 		tokens := throfflib.StringsToTokens(strs)
 		t.LoadTokens(tokens)
-		t=t.Run()
+		t = t.Run()
 		t.RunString("PRINTLN", "replprint")
+
 	}
 }
