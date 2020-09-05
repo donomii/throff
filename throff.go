5329ba4c8dff6e8a3c3931d693e74ee301bf1d3c
Jeremy Price <jeremy.price@careem.com>
2020-09-05 13:51:05 +0200

    Improve japanese prompt

--- throff.go
+++ throff.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-"log"
+	//"log"
 	"os"
 	"strings"
 
@@ -21,7 +21,7 @@ func main() {
 	//t = throfflib.LoadGraphics(t)  //Add this to throfflib so it can be called from Throff as a word?
 	//t = throfflib.LoadAudio(t)
 	strs := os.Args[1:]
-	log.Printf("Evaluating %v\n", strs)
+	//log.Printf("Evaluating %v\n", strs)
 	if len(strs) == 1 {
 		for _, arg := range strs {
 			if whatlanggo.Jpn == whatlanggo.Detect(arg).Lang {
@@ -34,9 +34,15 @@ func main() {
 	t = t.RunString(throfflib.BootStrapString(), "Internal Bootstrap")
 
 	if len(strs) == 0 {
-		t = t.RunString("PRINTLN [ Welcome to THROFF  v0.1.  Type HELP for help. ]", "repl")
+		//log.Printf("progname %v", os.Args[0])
+		if strings.Contains(os.Args[0], "小日本語") {
+			lang = "jp"
+			t = t.RunString("PRINTLN [ 小日本語　0.1.  使い方ヘップを入力しなさい ] PRINTLN NEWLINE ", "repl")
+		} else {
+			t = t.RunString("PRINTLN [ Welcome to Throff　v0.1.  Type HELP for help. ] PRINTLN NEWLINE ", "repl")
+		}
 		//t = t.RunString(" [ \nTHROFF v0.1.  Type HELP for help. ]", "repl")
-		throfflib.Repl(t)
+		throfflib.Repl(t, lang == "jp")
 	} else {
 		throfflib.PrintWarnings = false
 		if lang == "jp" {
